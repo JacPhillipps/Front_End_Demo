0059be125891635c826f44080c3b76917bdeddc1
Jac Phillipps <65624719+JacPhillipps@users.noreply.github.com>
2023-02-22 20:59:26 +0000

    Added Navbar

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg p-4">
+  <div class="container-fluid">
+    <a class="navbar-brand text-uppercase fw-bold" href="#">Mi Business</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item ms-5">
+          <a class="nav-link active text-uppercase" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link active text-uppercase" aria-current="page" href="#">Info</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link active text-uppercase" aria-current="page" href="#">Expetises</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link active text-uppercase" aria-current="page" href="#">Work</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link active text-uppercase" aria-current="page" href="#">Plans</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link active text-uppercase" aria-current="page" href="#">News</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+        <li class="nav-item me-4">
+          <a class="nav-link active text-uppercase" aria-current="page" href="#">Sing In</a>
+        </li>
+        <li class="nav-item me-3">
+          <a class="nav-link active text-uppercase" aria-current="page" href="#">Sing Up</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
