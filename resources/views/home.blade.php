111bc8433d9608d900d2ade6a970f908bce6b94a
Jac Phillipps <65624719+JacPhillipps@users.noreply.github.com>
2023-02-24 19:56:22 +0000

    Finished first page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,25 @@
 @extends ('app')
 @include('navbar')
 @section ('content')
-<div class="row p-4 overflow-auto">
-  <div class="col-1 ms-5 me-2">
+<div class="row">
+  <div class="col-1 ms-5 me-2 mt-5">
     <div class="row">
-      <a class="btn btn-round btn-outline-dark btn-floating mb-3 p-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+      <a class="btn btn-round btn-outline-dark btn-floating mb-3 p-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
           <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
         </svg>
       </a>
     </div>
     <div class="row">
-      <a class="btn btn-round btn-outline-dark btn-floating mb-3 p-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+      <a class="btn btn-round btn-outline-dark btn-floating mb-3 p-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
           <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
         </svg>
       </a>
     </div>
     <div class="row">
-      <a class="btn btn-round btn-outline-dark btn-floating mb-3 p-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-pinterest" viewBox="0 0 16 16">
+      <a class="btn btn-round btn-outline-dark btn-floating mb-3 p-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pinterest" viewBox="0 0 16 16">
           <path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z"/>
         </svg>
       </a>
@@ -27,9 +27,9 @@
   </div>
 
 
-  <div class="col-4 ms-5">
+  <div class="col-4 ms-5 mt-4">
     <div class="row">
-      <h1 class="fw-bold display-5 title" id="heading1">How do you design content for every language</h1>
+      <h1 class="fw-bold display-5 title" id="home">How do you design content for every language</h1>
     </div>
     <div class="row my-3">
       <p class="fw-lighter subtitle">We're the content of the research and design at Oxford. We help riders move, restaurants grow and customers eat in over 200 cities around the world. Come find the answers with us</p>
@@ -38,25 +38,30 @@
       <a class="btn btn-work btn-round btn-dark btn-floating ms-2 mb-3 text-white">Work with us</a>
     </div>
     <div class="row">
-      <a class="btn btn-circle btn-outline-light rounded-circle btn-floating ms-2 mb-3 text-black py-4">Scroll</a>
+      <a class="btn border border-secondary btn-circle btn-outline-light rounded-circle btn-floating ms-2 mt-5 text-black py-4">Scroll</a>
     </div>
   </div>
 
-  <div class="col-5 mt-auto ms-auto end">
-    <div class="blue_image float-end">
-      <img class="image" src="{{url('img/laptop_person.png')}}" alt="description of myimage" >
+  <div class="col-5 overflow ms-auto">
+    <div class="blue_image">
+      <img class="image" src="{{url('img/laptop_person.png')}}" alt="A picture of someone on a laptop" >
     </div>
   </div>
 
   <div class="row">
-    <div class="col-8 background filtered bg-image text-white p-5">
-      <div class="col-6 ms-5 mt-5 mb-2">
-        <h2 id="heading2" class="h3 mb-2">Delighting customers in over 200 cities in the world.</h2>
+    <div class="blue_image2">
+      <div class="col-12 background bg-image text-white p-5">
+        <div class="col-4 ms-5 mt-5 mb-2">
+          <h2 id="info" class="h3 mb-2 fw-bold">Delighting customers in over 200 cities in the world.</h2>
 
-        <p class="mb-2">We're the content, research and design team at Oxford. We help riders move, restaurants grow and customers eat in over 200 cities around the world. Come find the answer with us.</p>
+          <p class="mb-2">We're the content, research and design team at Oxford. We help riders move, restaurants grow and customers eat in over 200 cities around the world. Come find the answer with us.</p>
 
-        <a class="btn btn-work btn-round btn-dark btn-floating ms-2 my-3 text-white">Find Out More</a>
+          <a class="btn btn-work btn-round btn-dark btn-floating ms-2 my-3 text-white">Find Out More</a>
+        </div>
       </div>
+    </div>
+    <div class="container overflow">
+      <img class="image_width absolute" src="{{url('img/3d_shapes.png')}}" alt="A graphic of 3d shapes" >
     </div>
   </div>
 
@@ -64,7 +69,7 @@
     <div class="col pb-3 px-5">
       <div class="col-6 mx-auto">
         <div class="row mb-4">
-          <h2 class="title" id="heading3" >Our Expertises</h2>
+          <h2 class="title" id="expertises" >Our Expertises</h2>
         </div>
         <div class="row">
           <p class="subtitle">We're the content, research and design team at Oxford. We help riders move, restaurants grow and customers eat in over 200 cities around the world. Come find the answer with us.</p>        
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,36 +1,36 @@
-<nav class="navbar navbar-expand-lg p-4">
-  <div class="container-fluid">
-    <a class="navbar-brand text-uppercase fw-bold" href="#">Mi Business</a>
+<nav class="navbar navbar-expand-lg p-4 overflow">
+  <div class="container-fluid font">
+    <a class="navbar-brand blue_hover text-uppercase fw-bold brand" href="#">Mi Business</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item ms-5">
-          <a class="nav-link text-uppercase" aria-current="page" href="#heading1">Home</a>
+          <a class="nav-link blue_hover text-uppercase" aria-current="page" href="#home">Home</a>
         </li>
         <li class="nav-item ms-3">
-          <a class="nav-link text-uppercase" aria-current="page" href="#heading2">Info</a>
+          <a class="nav-link blue_hover text-uppercase" aria-current="page" href="#info">Info</a>
         </li>
         <li class="nav-item ms-3">
-          <a class="nav-link text-uppercase" aria-current="page" href="#heading3">Expertises</a>
+          <a class="nav-link blue_hover text-uppercase" aria-current="page" href="#expertises">Expertises</a>
         </li>
         <li class="nav-item ms-3">
-          <a class="nav-link text-uppercase" aria-current="page" href="#heading4">Work</a>
+          <a class="nav-link blue_hover text-uppercase" aria-current="page" href="#work">Work</a>
         </li>
         <li class="nav-item ms-3">
-          <a class="nav-link text-uppercase" aria-current="page" href="#heading5">Plans</a>
+          <a class="nav-link blue_hover text-uppercase" aria-current="page" href="#plans">Plans</a>
         </li>
         <li class="nav-item ms-3">
-          <a class="nav-link text-uppercase" aria-current="page" href="#heading6">News</a>
+          <a class="nav-link blue_hover text-uppercase" aria-current="page" href="#news">News</a>
         </li>
       </ul>
       <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
         <li class="nav-item me-4">
-          <a class="nav-link active text-uppercase" aria-current="page" href="#">Sing In</a>
+          <a class="nav-link blue_hover active text-uppercase" aria-current="page" href="#">Sing In</a>
         </li>
         <li class="nav-item me-3">
-          <a class="nav-link active text-uppercase" aria-current="page" href="#">Sing Up</a>
+          <a class="nav-link blue_hover active text-uppercase" aria-current="page" href="#">Sing Up</a>
         </li>
       </ul>
 
